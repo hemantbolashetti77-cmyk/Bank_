3eb1af3ecba90fee3f19f21c86039465d4aa2b35
hemant17 <hemantbolashetti77@gmail.com>
2025-11-27 15:49:19 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -3,12 +3,14 @@ import sys
 
 if len(sys.argv) >= 3:
   script_name = sys.argv[0]
-  intial = sys.argv[1]
+  initial = sys.argv[1]
   deposit = sys.argv[2]
 else:
   script_name = sys.argv[0]
   initial= 1500
   deposit=2000
 balance=initial+deposit
+print("initial Amount:",initial)
+print("deposit Amount:",deposit)
 print("Balance Amount:",balance)
  
